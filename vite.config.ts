bf739836191d261ee43995f268ddcca2cc8534eb
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2024-09-25 20:47:28 +0330

    add vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Intervals',
+        short_name: 'Intervals',
+        description: 'Music Interval Training',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
 })
